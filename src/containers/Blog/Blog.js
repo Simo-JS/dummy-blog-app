2c88d44e07d1bc1547fa50e263c26887b018cb51
SimoJS <mohamed.akraich@gmail.com>
2019-09-01 14:44:04 +0100

    Fetching blog posts from a dummy REST API

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,17 +5,42 @@ import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
+import axios from "axios";
+
 class Blog extends Component {
+  state = {
+    posts: [],
+    id: null
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      const posts = response.data.slice(0, 6);
+      const updatedPosts = posts.map(post => {
+        return { ...post, author: "Simo" };
+      });
+      this.setState({ posts: updatedPosts });
+    });
+  }
+
+  selectedPostHander = id => {
+    this.setState({ id: id });
+  };
+
   render() {
+    let posts = this.state.posts.map(post => (
+      <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.selectedPostHander(post.id)}
+      />
+    ));
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.id} />
         </section>
         <section>
           <NewPost />
